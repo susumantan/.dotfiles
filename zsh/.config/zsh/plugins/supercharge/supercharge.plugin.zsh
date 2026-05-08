06cffd01126607510e8c7e38f21f2a2f6d488c6b
SusuMantan <suzumantan@gmail.com>
2026-05-08 23:56:52 +0700

    update

--- zsh/.config/zsh/plugins/supercharge/supercharge.plugin.zsh
+++ zsh/.config/zsh/plugins/supercharge/supercharge.plugin.zsh
@@ -1,24 +1,24 @@
 # completions
-# autoload -Uz compinit
-# zstyle ':completion:*' menu select
-# zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
-# zstyle ':completion:*' list-colors 'di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
-# zmodload zsh/complist
-# _comp_options+=(globdots)		# Include hidden files.
-# zle_highlight=('paste:none')
-# for dump in "${ZDOTDIR:-$HOME}/.zcompdump"(N.mh+24); do
-#   compinit
-# done
-# compinit -C
+autoload -Uz compinit
+zstyle ':completion:*' menu select
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*' list-colors 'di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+zmodload zsh/complist
+_comp_options+=(globdots)		# Include hidden files.
+zle_highlight=('paste:none')
+for dump in "${ZDOTDIR:-$HOME}/.zcompdump"(N.mh+24); do
+  compinit
+done
+compinit -C
 
 # unsetopt BEEP
-# setopt AUTO_CD
-# setopt GLOB_DOTS
-# setopt NOMATCH
-# setopt AUTO_MENU
+setopt AUTO_CD
+setopt GLOB_DOTS
+setopt NOMATCH
+setopt AUTO_MENU
 # setopt MENU_COMPLETE
-# setopt EXTENDED_GLOB
-# setopt INTERACTIVE_COMMENTS
+setopt EXTENDED_GLOB
+setopt INTERACTIVE_COMMENTS
 
 # Colors
 autoload -Uz colors && colors
@@ -29,20 +29,20 @@ export PATH="$HOME/.local/bin:$PATH"
 # bindings
 
 # Use emacs key bindings
-# bindkey -e
+bindkey -e
 
 # Edit the current command line in $EDITOR
-# autoload -U edit-command-line
-# zle -N edit-command-line
-# bindkey '\C-x\C-e' edit-command-line
+autoload -U edit-command-line
+zle -N edit-command-line
+bindkey '\C-x\C-e' edit-command-line
 
 # file rename magick
-# bindkey "^[m" copy-prev-shell-word
+bindkey "^[m" copy-prev-shell-word
 
-# bindkey '^xe' edit-command-line
-# bindkey '^x^e' edit-command-line
+bindkey '^xe' edit-command-line
+bindkey '^x^e' edit-command-line
 
-# bindkey -M menuselect '^[[Z' reverse-menu-complete
+bindkey -M menuselect '^[[Z' reverse-menu-complete
 # bindkey -s '^x' '^usource $ZSHRC\n'
 # bindkey -M menuselect '?' history-incremental-search-forward
 # bindkey -M menuselect '/' history-incremental-search-backward
